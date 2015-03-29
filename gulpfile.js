627202b9fff5abc206b760c85310db42e860d157
Jasper Woudenberg <jasper.woudenberg@icemobile.com>
2015-03-29 19:18:37 +0200

    Initial project setup.
    Using browserify and babel.

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var sourcemaps = require('gulp-sourcemaps');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var watchify = require('watchify');
+var browserify = require('browserify');
+var babelify = require('babelify');
+
+var bundler = watchify(browserify(watchify.args));
+bundler.add('./src/index.js');
+bundler.transform(babelify);
+
+gulp.task('js', bundle);
+bundler.on('log', gutil.log);
+
+function bundle() {
+    return bundler.bundle()
+        .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./dist'));
+}
