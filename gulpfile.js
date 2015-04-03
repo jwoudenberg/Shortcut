b2352d964ccaa373ec3bc5b7f9b51f3922177047
Jasper Woudenberg <jasper.woudenberg@icemobile.com>
2015-04-03 19:39:49 +0200

    Added sass compilation.

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,49 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var sourcemaps = require('gulp-sourcemaps');
+var watch = require('gulp-watch');
+var sass = require('gulp-sass');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var watchify = require('watchify');
 var browserify = require('browserify');
 var babelify = require('babelify');
 
-var bundler = watchify(browserify(watchify.args));
-bundler.add('./src/index.js');
-bundler.transform(babelify);
+gulp.task('js', function watchJs() {
+    var bundler = watchify(browserify(watchify.args));
+    bundler.add('./src/index.js');
+    bundler.transform(babelify);
+    bundler.on('update', bundle);
+    bundler.on('log', gutil.log);
+    return bundle();
 
-gulp.task('js', bundle);
-bundler.on('update', bundle);
-bundler.on('log', gutil.log);
+    function bundle() {
+        return bundler.bundle()
+            .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+            .pipe(source('bundle.js'))
+            .pipe(buffer())
+            .pipe(sourcemaps.init({loadMaps: true}))
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest('./dist'));
+    }
+});
 
-function bundle() {
-    return bundler.bundle()
-        .on('error', gutil.log.bind(gutil, 'Browserify Error'))
-        .pipe(source('bundle.js'))
-        .pipe(buffer())
-        .pipe(sourcemaps.init({loadMaps: true}))
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./dist'));
-}
+gulp.task('sass', function watchSass() {
+    var STYLE_PATH = './style/**/*.scss';
+    watch(STYLE_PATH, bundle);
+
+    return bundle();
+
+    function bundle() {
+        return gulp.src(STYLE_PATH)
+            .pipe(sourcemaps.init())
+            .pipe(sass({
+                onSuccess: function logResult() { console.log('SASS recompiled.'); },
+                errLogToConsole: true
+            }))
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest('./dist'));
+    }
+});
+
+gulp.task('default', ['js', 'sass']);
