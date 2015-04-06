6ac751d5d494092e51754cf598a6bc599e235558
Jasper Woudenberg <jasper.woudenberg@icemobile.com>
2015-04-06 21:16:46 +0200

    Added kefir and uiEventStream.
    The stream will be shared among UI components, which will push events
    there. So far the GameCreator is the only view using it.

--- src/view/page.js
+++ src/view/page.js
@@ -1,21 +1,52 @@
 const React = require('react');
+var R = require('ramda');
+const uiEventStream = require('./ui-event-stream');
 
 class GameCreator extends React.Component {
+    constructor() {
+        //TODO: Consider moving state out of this component.
+        this.state = {
+            numberOfPlayers: 2,
+            boardSize: 4
+        };
+    }
+    handleBoardSizeChange(event) {
+        let boardSize = parseInt(event.target.value);
+        if (Number.isNaN(boardSize) || boardSize < 2) {
+            return null;
+        }
+        this.setState({ boardSize });
+        this.sendCreateGameEvent();
+    }
+    handleNumberOfPlayersChange(event) {
+        let numberOfPlayers = parseInt(event.target.value);
+        if (Number.isNaN(numberOfPlayers) || numberOfPlayers < 2) {
+            return null;
+        }
+        this.setState({ numberOfPlayers });
+        this.sendCreateGameEvent();
+    }
+    sendCreateGameEvent() {
+        let gameEvent = R.merge(this.state, {
+            action: 'create_game'
+        });
+        uiEventStream.emit(gameEvent);
+    }
     render() {
-
+        let { numberOfPlayers, boardSize } = this.state;
         return <form className="game-creator navbar-form navbar-left" role="create-game">
             <div className="form-group">
-                <label className="sr-only" for="number-of-players">Number of players</label>
                 <div className="input-group">
                     <div className="input-group-addon">Players:</div>
-                    <input className="form-control" name="number-of-players" min="2" type="number" value="2" />
+                    <input className="form-control" name="numberOfPlayers" min="2" type="number"
+                        value={numberOfPlayers} onChange={this.handleNumberOfPlayersChange.bind(this)}/>
                 </div>
             </div>
             <div className="form-group">
-                <label className="sr-only" for="board-size">Board size</label>
                 <div className="input-group">
                     <div className="input-group-addon">Board Size:</div>
-                    <input className="form-control" name="board-size" min="2" type="number" value="4" />
+                    <input className="form-control" name="boardSize" min="2" type="number" value={boardSize}
+                        onChange={this.handleBoardSizeChange.bind(this)}/>
                 </div>
             </div>
             <input name="start-game" type="button" className="btn btn-primary" value="Start Game" />
---
+++ src/view/ui-event-stream.js
@@ -0,0 +1,5 @@
+var Kefir = require('kefir');
+
+let uiEventStream = Kefir.emitter();
+
+module.exports = uiEventStream;
