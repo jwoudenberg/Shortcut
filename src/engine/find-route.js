fd3662d3f2c9d36bf6aa21de73de8773d285cc25
Jasper Woudenberg <jasper.woudenberg@icemobile.com>
2015-08-01 14:18:08 +0200

    Fixed issue with route finding on deck card.

--- src/engine/find-route.js
+++ src/engine/find-route.js
@@ -3,15 +3,15 @@ import { fromJS, Map, Set, List } from 'immutable';
 
 let lastWorldState, paths;
 export default function findRoute(mutablePathId, mutableWorldState) {
-    let worldState = fromJS(mutableWorldState);
+    const worldState = fromJS(mutableWorldState);
     //DEBUG: store the last world state, so the paths calculation for subsequent calls on the same world only happens
     //once.
     if (!worldState.equals(lastWorldState)) {
         paths = getQueryablePaths(worldState);
         lastWorldState = worldState;
     }
-    let pathId = Map(mutablePathId);
-    let _getNeighbourPaths = R.flip(getNeighbourPaths)(paths);
+    const pathId = Map(mutablePathId);
+    const _getNeighbourPaths = R.flip(getNeighbourPaths)(paths);
     function findRouteRecusive(pathId, seenPathIds=Set()) {
         //Check if we've already seen this path.
         if (seenPathIds.has(pathId)) {
@@ -36,8 +36,8 @@ function getNeighbourPaths(pathId, paths) {
 }
 
 function getNeighbourCoords(coords) {
-    let direction = Math.floor(coords.get('port') / 2);
-    let isEven = (number) => (number % 2) === 0;
+    const direction = Math.floor(coords.get('port') / 2);
+    const isEven = (number) => (number % 2) === 0;
     return coords
         .update('row', [R.inc, R.identity, R.dec, R.identity][direction])
         .update('col', [R.identity, R.inc, R.identity, R.dec][direction])
@@ -52,18 +52,18 @@ function getNeighbourCoords(coords) {
 }
 
 function getQueryablePaths(worldState) {
-    let fields = worldState.getIn(['board', 'fields'], List());
-    let coordsByFieldId = new Map(
+    const fields = worldState.getIn(['board', 'fields'], List());
+    const coordsByFieldId = new Map(
         fields.map(field => [field.get('id'), { row: field.get('row'), col: field.get('col') }])
     );
-    let cards = worldState.get('cards', List()).map(getEquivalentNonRotatedCard);
-    let coordsByPathId = Map(
+    const cards = worldState.get('cards', List()).map(getEquivalentNonRotatedCard);
+    const coordsByPathId = Map(
         cards.flatMap(
             card =>  card.get('paths', List()).map(
                 (path, pathIndex) =>  {
-                    let ports = path.get('ports', List());
-                    let cardId = card.get('id');
-                    let { row, col } = coordsByFieldId.get(card.get('field'));
+                    const ports = path.get('ports', List());
+                    const cardId = card.get('id');
+                    const { row, col } = coordsByFieldId.get(card.get('field')) || {};
                     return [
                         Map({ pathIndex, cardId }),
                         ports.map(port => Map({ port, row, col }))
@@ -72,7 +72,7 @@ function getQueryablePaths(worldState) {
             )
         )
     );
-    let pathIdByCoords = coordsByPathId.flatMap(
+    const pathIdByCoords = coordsByPathId.flatMap(
         (coords, pathId) => Map(
             coords.map(coord => [coord, pathId])
         )
@@ -84,7 +84,7 @@ function getQueryablePaths(worldState) {
 }
 
 function getEquivalentNonRotatedCard(card) {
-    let rotation = card.get('rotation');
+    const rotation = card.get('rotation');
     return card
         .set('rotation', 0)
         .update('paths', List(), (paths) => paths.map(
